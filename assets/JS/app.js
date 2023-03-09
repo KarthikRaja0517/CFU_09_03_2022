9dcc7df93f1abb9f6b755a22c0b9dc130fcd4717
KarthikRaja0517 <116252119+KarthikRaja0517@users.noreply.github.com>
2023-03-09 18:20:05 +0530

    Worked on it

---
+++ assets/JS/app.js
@@ -0,0 +1,24 @@
+
+/*get the number of characters*/
+function charLength() {
+
+    let charLength = document.getElementById('length').value;
+    document.getElementById('result').innerHTML = charLength;
+    return charLength;
+
+}
+
+document.getElementById('length').addEventListener('change', charLength)
+// uppercase letter
+let allchars = “0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ”;
+let Uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let UpperLowercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+function upperCase(){
+for (let i = 0; i < uppercase_letters.length; i++) {
+
+}
+
+
+}
+document.getElementById('uppercase').addEventListener('change', upperCase)
+
